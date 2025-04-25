be1252e1be0a89b54544b69f743a89238e5a3751
Solhvemjsun <solhvemjsun@gmail.com>
2025-04-25 04:23:10 -0700

    Update laptop.nix
    Added power management

--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -8,4 +8,31 @@
   services.libinput.enable = true; # Enable touchpad
 
   services.fprintd.enable = true; # Enable fingerprint reader
+
+  ######################
+  ## POWER MANAGEMENT ##
+  ######################
+
+  powerManagement.enable = true;
+
+  services.thermald.enable = true; # Prevents CPU from overheating
+
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        energy_pref_bias = "balance_power";
+        platform_profile = "balanced";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        energy_pref_bias = "balance_performance";
+        platform_profile = "performance";
+        turbo = "auto";
+      };
+    };
+  };
+
 }
