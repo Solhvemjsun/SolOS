7a2bd732de30236d11f9bb757bb4799f89d55b1b
Solhvemjsun <solhvemjsun@gmail.com>
2025-04-25 04:43:44 -0700

    Update hyprland.nix
    Updated fcitx5 config

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -222,11 +222,8 @@
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
-      fcitx5-chinese-addons
-      fcitx5-pinyin-zhwiki
-      fcitx5-pinyin-moegirl
-      fcitx5-pinyin-minecraft
       fcitx5-rime
+      fcitx5-mozc
       rime-ls
       rime-data
       rime-zhwiki
