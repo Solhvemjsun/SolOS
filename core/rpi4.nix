6546f2bfdb996abafae3412f7839d828613400ef
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-07 03:04:42 -0700

    Update rpi4.nix

--- core/rpi4.nix
+++ core/rpi4.nix
@@ -2,7 +2,7 @@
 
 {
   boot = lib.mkForce {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_rpi4;
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
