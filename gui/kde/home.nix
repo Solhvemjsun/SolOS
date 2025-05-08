e4eb3f7e5331f5ec5b28907313e887391d6bce5a
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-08 00:55:35 -0700

    Update home.nix

--- gui/kde/home.nix
+++ gui/kde/home.nix
@@ -18,23 +18,23 @@
   ## XDG ##
   #########
 
-  xdg = {
-    userDirs = {
-      enable = true;
-      music = "${config.home.homeDirectory}/Nextcloud/Music";
-      pictures = "${config.home.homeDirectory}/Nextcloud/Pictures";
-      videos = "${config.home.homeDirectory}/Nextcloud/Videos";
-      documents = "${config.home.homeDirectory}/Documents";
-      download = "${config.home.homeDirectory}/Downloads";
-      desktop = "${config.home.homeDirectory}/Desktop";
-    };
+  # xdg = {
+  #   userDirs = {
+  #     enable = true;
+  #     music = "${config.home.homeDirectory}/Nextcloud/Music";
+  #     pictures = "${config.home.homeDirectory}/Nextcloud/Pictures";
+  #     videos = "${config.home.homeDirectory}/Nextcloud/Videos";
+  #     documents = "${config.home.homeDirectory}/Documents";
+  #     download = "${config.home.homeDirectory}/Downloads";
+  #     desktop = "${config.home.homeDirectory}/Desktop";
+  #   };
     # mimeApps = {
     #   enable = true;
     #   defaultApplications = {
     #     "x-terminal-emulator" = ["kitty.desktop"];
     #   };
     # };
-  };
+  # };
 
   ###########
   ## SHELL ##
