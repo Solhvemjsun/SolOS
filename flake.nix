948036c8323b55fd60d21eed3eb3a3a114920811
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-08 04:38:55 -0700

    Again

--- flake.nix
+++ flake.nix
@@ -99,7 +99,7 @@
 
       "MachenikeMini" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = commonModules ++ terminalModules ++ [
+        modules = commonModules ++ terminalModules ++ kdeModules ++ personalModules ++ [
           {
             networking.hostName = "MachenikeMini";
           }
