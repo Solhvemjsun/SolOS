03d2d3a82bde5d207fdd31c1e8dbe734419d897d
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-08 04:46:52 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     home-manager.url = "github:nix-community/home-manager";
-    plasma-manager.url = "github:nix-community/plasma-manager";
+    # plasma-manager.url = "github:nix-community/plasma-manager";
     stylix.url = "github:danth/stylix";
     minegrub-theme.url = "github:Lxtharia/minegrub-theme";
   };
@@ -32,7 +32,7 @@
     kdeModules = [
       minegrub-theme.nixosModules.default
       home-manager.nixosModules.home-manager
-      plasma-manager.homeManagerModules.plasma-manager
+      # plasma-manager.homeManagerModules.plasma-manager
       ./gui/common.nix
       ./gui/kde/kde.nix
     ];
