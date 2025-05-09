3b1f02ded2100ef98839f9c39021c99ab6037812
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-09 00:51:34 -0700

    again

--- system/nix-on-droid.nix
+++ system/nix-on-droid.nix
@@ -8,4 +8,8 @@
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
+  home-manager.config = { config, lib, pkgs, ... }:
+  {
+    home.stateVersion = "24.05";
+  };
 }
