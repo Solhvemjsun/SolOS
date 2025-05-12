d0c52940561cbd77f08739b397b458db0fe24ccb
Solhvemjsun <solhvemjsun@gmail.com>
2025-05-11 20:48:42 -0700

    Update nix-on-droid.nix

--- core/nix-on-droid.nix
+++ core/nix-on-droid.nix
@@ -61,24 +61,6 @@
 
   stylix = {
     enable = true;
-    fonts = {
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-      monospace = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans Mono";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
     polarity = "dark";
     base16Scheme = {
       system = "base24";
@@ -111,6 +93,7 @@
       base17 = "ff00ff";
     };
     targets = {
+      vim = false;
     };
   };
 }
