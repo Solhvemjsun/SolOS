1d6478f37c5d12a54234ca17ef68997acb5af86f
Solhvemjsun <solhvemjsun@gmail.com>
2025-07-29 19:54:14 -0700

    Disabled OSProber on XPS13

--- device/XPS13/device-specific.nix
+++ device/XPS13/device-specific.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [ ./hardware-configuration.nix ];
+  boot.loader.grub.useOSProber = lib.mkForce false;
   home-manager.sharedModules = [
     {
       programs.fuzzel.settings.main = {
