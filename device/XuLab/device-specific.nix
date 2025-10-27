91ce33a5f60bae2c2537d5c70931ae53c72dd295
Solhvemjsun <Solhvemjsun@gmail.com>
2025-10-27 16:30:34 -0700

    Repair the extra vram for xulab PC

--- device/XuLab/device-specific.nix
+++ device/XuLab/device-specific.nix
@@ -4,4 +4,11 @@
   imports = [ ./hardware-configuration.nix ];
 
   networking.hostName = "XuLab";
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 1024*1024;
+    }
+  ];
 }
