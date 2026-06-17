9d0d3842cf2e124ad877ccdcd933f3f7ff66ee4d
Sol <solhvemjsun@gmail.com>
2026-06-17 16:36:53 +0800

    Enhanced input method

--- mods/gui/common.nix
+++ mods/gui/common.nix
@@ -185,13 +185,13 @@
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
-      fcitx5-gtk
+      qt6Packages.fcitx5-chinese-addons
       fcitx5-rime
       fcitx5-mozc
-      rime-ls
-      rime-data
-      rime-zhwiki
-      rime-japanese
+      fcitx5-material-color
+      fcitx5-pinyin-zhwiki
+      fcitx5-pinyin-moegirl
+      fcitx5-pinyin-minecraft
     ];
   };
 
