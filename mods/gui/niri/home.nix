3538a5472e79b6d4e1a39ae13ab023ccf51f17a7
Sol <solhvemjsun@gmail.com>
2026-06-17 16:37:11 +0800

    Added new screen configuration

--- mods/gui/niri/home.nix
+++ mods/gui/niri/home.nix
@@ -184,6 +184,38 @@
           backdrop-color = "#000000";
         };
 
+        "Albert_Alienware" = {
+          name = "Dell Inc. AW3423DWF G8672S3";
+          mode = {
+            width = 3440;
+            height = 1440;
+            refresh = 120.000;
+          };
+          scale = 1.0;
+          position = {
+            x = 2560;
+            y = 0;
+          };
+          background-color = "#000000";
+          backdrop-color = "#000000";
+        };
+
+        "Xreal" = {
+          name = "Nreal Air 2 Pro 0x88888800";
+          mode = {
+            width = 1920;
+            height = 1080;
+            refresh = 120.000;
+          };
+          scale = 1.0;
+          position = {
+            x = -1920;
+            y = 0;
+          };
+          background-color = "#000000";
+          backdrop-color = "#000000";
+        };
+
       };
 
       layout = {
